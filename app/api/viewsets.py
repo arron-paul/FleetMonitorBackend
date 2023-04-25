9d7b36eb4870527f15967400e448e93a8743a3f2
Arron <hello@arron.io>
2023-04-25 09:13:16 +0000

    Removed unnessessary viewset overrides

--- app/api/viewsets.py
+++ app/api/viewsets.py
@@ -1,6 +1,4 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from app.api.filters import SensorRecordFilter
 from app.api.serializers import SensorSerializer, SensorRecordSerializer
@@ -14,15 +12,6 @@ class SensorViewSet(viewsets.ModelViewSet):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
 
-    def list(self, request, **kwargs):
-        serializer = SensorSerializer(self.queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None, **kwargs):
-        sensor = get_object_or_404(self.queryset, pk=pk)
-        serializer = SensorSerializer(sensor)
-        return Response(serializer.data)
-
 
 class SensorRecordViewSet(viewsets.ModelViewSet):
     """
@@ -31,12 +20,3 @@ class SensorRecordViewSet(viewsets.ModelViewSet):
     queryset = SensorRecord.objects.all()
     serializer_class = SensorRecordSerializer
     filterset_class = SensorRecordFilter
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED,
-            headers=self.get_success_headers(serializer.data))
